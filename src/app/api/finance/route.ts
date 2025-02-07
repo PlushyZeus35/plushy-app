a828dede5055801a9ef5fadd875c8483b97ab53d
x391279 <x391279@gruposantander.es>
2025-02-07 14:42:22 +0100

    initial dev

--- src/app/api/finance/route.ts
+++ src/app/api/finance/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: Request) {
         });
     
         if (!response.ok) {
-          return NextResponse.json({ error: `Error en Strapi: ${response.statusText} ${JSON.stringify(await response.json())} ${STRAPI_URL}/api/finanzas ${JSON.stringify(records)}` }, { status: response.status });
+          return NextResponse.json({ error: `Error en Strapi: ${response.statusText} ${JSON.stringify(await response.json())} ${API_KEY} ${STRAPI_URL}/api/finanzas ${JSON.stringify(records)}` }, { status: response.status });
         }
     
         const result = await response.json();
